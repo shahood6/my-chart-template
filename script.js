5de2c4ceeee0e2a8527447489f74a5dc801da57d
shahood6 <fo1085qk@go.minnstate.edu>
2025-10-07 11:37:08 -0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var TITLE = 'English Learners by Select School Districts in CT, 2018-19';
+  var TITLE = 'Weekly Time Allocation';
 
   // `false` for vertical column chart, `true` for horizontal bar chart
   var HORIZONTAL = false;
@@ -26,10 +26,10 @@ $(document).ready(function() {
   ];
 
   // x-axis label and label in tooltip
-  var X_AXIS = 'School Districts';
+  var X_AXIS = 'Activities';
 
   // y-axis label, label in tooltip
-  var Y_AXIS = 'Number of Enrolled Students';
+  var Y_AXIS = 'Hours per Week';
 
   // `true` to show the grid, `false` to hide
   var SHOW_GRID = true; 
